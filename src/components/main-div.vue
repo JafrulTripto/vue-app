2d39e8e35aaf7ff5a9b791c71e611dae931f5a6e
Tripto <tripto_ewu@outlook.com>
2019-02-06 23:53:08 +0600

    first-commit

---
+++ src/components/main-div.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "main-div"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/my-components.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "my-components"
+    }
+</script>
+
+<style scoped>
+
+</style>
