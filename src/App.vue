11996249c0cd114b05ed46c850ef0365c17ffcdf
Safara IT-PC <tripto1992@gmail.com>
2019-02-07 14:57:16 +0600

    at 2:57

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
     <h1>To-Do</h1>
-    <main-file></main-file>
+    <Todo :lists="lists"></Todo>
 
   </div>
 
 </template>
 
 <script>
-  import main from './components/main-card'
+  import card from './components/Card-detail';
+  import main from './components/main-card';
+  import todoGrid from './components/todo-grid';
   export default {
     components:{
-      'main-file':main
+      mainFile:main,
+      cardDetail:card,
+      todo: todoGrid
+    },
+    data:function () {
+      return{
+          lists:[]
+      }
+    },
+    methods:{
+      newCard(newParagraph){
+        this.lists.push(newParagraph);
+      }
     }
 
   };
+
+  // :paragraph="newParagraph"
 </script>
 
 <style>
